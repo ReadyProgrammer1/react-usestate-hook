9d516d6809a94ce75d4e8b88ec1347e2820c8be9
Daniel Payne <42071158+ReadyProgrammer1@users.noreply.github.com>
2018-11-06 10:29:07 -0500

    Add files via upload

---
+++ precache-manifest.e84feeb06a61cf6e097630d794207473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1017d2b9c8c25d330127",
+    "url": "/react-usestate-hook/static/js/runtime~main.1017d2b9.js"
+  },
+  {
+    "revision": "12c99aac5b4b0c947810",
+    "url": "/react-usestate-hook/static/js/main.12c99aac.chunk.js"
+  },
+  {
+    "revision": "449f3182d0edcd2d0f38",
+    "url": "/react-usestate-hook/static/js/1.449f3182.chunk.js"
+  },
+  {
+    "revision": "12c99aac5b4b0c947810",
+    "url": "/react-usestate-hook/static/css/main.fdebc54f.chunk.css"
+  },
+  {
+    "revision": "fda7db33ef25d7c2d9d8cf1d73d3d79d",
+    "url": "/react-usestate-hook/index.html"
+  }
+];
